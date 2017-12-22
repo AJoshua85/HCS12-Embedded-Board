27256af7ba8673389fb8b33b6d677799ef1eb116
mluong0142 <mluong0142@conestogac.on.ca>
2017-12-21 22:30:02 -0500

    Update with Heartbeat
    For Arish

--- Modules/server.c
+++ Modules/server.c
@@ -53,11 +53,14 @@ int openPort(void)
 int main (void){
 
 
+	//sleep(1);
+	int checkRestart = 0;
 	char buffer[BUFSIZ];	// Data buffer for communications
 	int server_socket, client_socket;
 	int client_len;
 	struct sockaddr_in client_addr, server_addr;
 	int len;
+	char tbuffer[3] = "\0";//Buffer used to check if that data in the serial port
 
 	/*  Linux RS-232  */
 	int mainfd = 0;
@@ -67,72 +70,76 @@ int main (void){
     char *serialOut;// pointer used to create buffer including the testring with flags
 	
 	int n;//used for error checking 
-	int checkReturn;//used to check with data is in the serial port
+	int checkReturn = 0;//used to check with data is in the serial port
     
-        //Serial Settings
-	mainfd =openPort();
-	fcntl(mainfd,F_SETFL,FNDELAY);
 
-	tcgetattr(mainfd,&options);
-	cfsetispeed(&options,B9600);
-	cfsetospeed(&options,B9600);
+    
 
-	options.c_cflag |= (CLOCAL|CREAD);
-	options.c_cflag &= ~PARENB; /* Mask the character size to 8 bits, no parity */
-	options.c_cflag &= ~CSTOPB;/* One stop bit*/
-	options.c_cflag &= ~CSIZE;
-	options.c_cflag |=  CS8;/* Select 8 data bits */
-	options.c_cflag &= ~CRTSCTS;//Disable hardware control
+	        //Serial Settings
+		mainfd =openPort();
+		fcntl(mainfd,F_SETFL,FNDELAY);
 
+		tcgetattr(mainfd,&options);
+		cfsetispeed(&options,B9600);
+		cfsetospeed(&options,B9600);
 
-	options.c_lflag &= ~(ICANON | ECHO | ISIG);/* Enable data to be processed as raw input */
-	tcsetattr(mainfd, TCSANOW, &options);/* Set the new options for the port */
-	int i =0;
+		options.c_cflag |= (CLOCAL|CREAD);
+		options.c_cflag &= ~PARENB; /* Mask the character size to 8 bits, no parity */
+		options.c_cflag &= ~CSTOPB;/* One stop bit*/
+		options.c_cflag &= ~CSIZE;
+		options.c_cflag |=  CS8;/* Select 8 data bits */
+		options.c_cflag &= ~CRTSCTS;//Disable hardware control
 
 
-	/* obtain a socket for the server */
+		options.c_lflag &= ~(ICANON | ECHO | ISIG);/* Enable data to be processed as raw input */
+		tcsetattr(mainfd, TCSANOW, &options);/* Set the new options for the port */
+		int i =0;
 
-	if ((server_socket = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
-		printf ("grrr, can't get the server socket\n");
-		return 1;
-	}	/* endif */
+		/* obtain a socket for the server */
 
-	/*
-	 * initialize our server address info for binding purposes
-	 */
+		if ((server_socket = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
+			printf ("grrr, can't get the server socket\n");
+			return 1;
+		}	/* endif */
 
-	memset (&server_addr, 0, sizeof (server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl (INADDR_ANY);
-	server_addr.sin_port = htons (PORT);
+		/*
+		 * initialize our server address info for binding purposes
+		 */
+
+		memset (&server_addr, 0, sizeof (server_addr));
+		server_addr.sin_family = AF_INET;
+		server_addr.sin_addr.s_addr = htonl (INADDR_ANY);
+		server_addr.sin_port = htons (PORT);
+		
+
+		if (bind (server_socket, (struct sockaddr *)&server_addr, 
+		sizeof (server_addr)) < 0) {
+			printf ("grrr, can't bind server socket\n");
+			close (server_socket);
+			return 2;
+		}	/* endif */
+
+		/* start listening on the socket */
+		if (listen (server_socket, 10) < 0) {
+			printf ("grrr, can't listen on socket\n");
+			close (server_socket);
+			return 3;
+		}	/* endif */
+
+		/* accept a packet from the client */
+		client_len = sizeof (client_addr);
+		if ((client_socket = accept (server_socket, 
+			(struct sockaddr *)&client_addr, &client_len)) < 0) {
+		  	printf ("grrr, can't accept a packet from client\n");
+		  	close (server_socket);
+		  	return 4;
+		}
+
 	
 
-	if (bind (server_socket, (struct sockaddr *)&server_addr, 
-	sizeof (server_addr)) < 0) {
-		printf ("grrr, can't bind server socket\n");
-		close (server_socket);
-		return 2;
-	}	/* endif */
-
-	/* start listening on the socket */
-	if (listen (server_socket, 10) < 0) {
-		printf ("grrr, can't listen on socket\n");
-		close (server_socket);
-		return 3;
-	}	/* endif */
-
-	/* accept a packet from the client */
-	client_len = sizeof (client_addr);
-	if ((client_socket = accept (server_socket, 
-	(struct sockaddr *)&client_addr, &client_len)) < 0) {
-	  printf ("grrr, can't accept a packet from client\n");
-	  close (server_socket);
-	return 4;
-	}
-
         while(1) {
-
           	memset(buffer, 0, sizeof(buffer));
+          	memset(tbuffer, '\0', sizeof(tbuffer));
 			read (client_socket, buffer, BUFSIZ);
 
 			serialOut = (char*)calloc(strlen(buffer) + 2,sizeof(char));	
@@ -144,12 +151,21 @@ int main (void){
        		 //put endflag after the msg
 			strncat(serialOut, ENDFLAG, 1);
 			write(mainfd,serialOut,strlen(serialOut));
-			sleep(1);
+			sleep(0.5);
 
 	  		printf("Message from client: %s ... %s\n", buffer, serialOut);
 	  		if (strcmp(buffer, "quit") == 0) {
 	    		printf("Client has requested to quit, exiting application\n");
 	    		break;
+	  		}
+	  		else if(strcmp(buffer, "") == 0){
+	  			printf("Client has been disconnected. Restarting Application...\n");
+	  			checkRestart = 1;
+	  			close (client_socket);
+	  			close (server_socket);
+	  			return main();
+				//free(serialOut);
+	  			break;
 	  		}
 
 	  		// write data to client 
@@ -158,10 +174,20 @@ int main (void){
 	    		printf("Exiting application\n");
 	    		break;
 	  		}
+	  		else if(strcmp(buffer, "")== 0){
+	  			printf("Exiting Due to Disconnections.\n");
+	  			break;
+	  		}
+
+	  		while(read(mainfd,tbuffer, sizeof(tbuffer)) != "\0"){	// Arish Heartbeat
+	  			write(mainfd, "k", 1);
+	  		}
+
 	  		memset(serialOut, 0, sizeof(serialOut));
 		}
 
 	free(serialOut);
-	close (client_socket); 
+	close (client_socket);
+	 
 	return 0;
 }
